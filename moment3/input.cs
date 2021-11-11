5130e3d1a87e90524e0fca08be43c2ef51533c22
Meuons <89832844+Meuons@users.noreply.github.com>
2021-11-11 14:59:14 +0100

    Add files via upload

---
+++ moment3/input.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.IO;
+
+namespace moment3
+{
+    
+    public class input
+    {
+       public string name;
+      public  string content;
+       public string list;
+        public int length;
+
+        public static void Write(string list, int length)
+        {
+            
+            input inputObj = new input();
+             inputObj.list = list;
+            inputObj.length = length;
+            Console.WriteLine("Write your name: ");
+            
+           
+            inputObj.name = Console.ReadLine();
+
+            Console.WriteLine("Write down a quote: "); 
+            inputObj.content = Console.ReadLine();
+
+
+
+            if (string.IsNullOrEmpty(inputObj.content) || string.IsNullOrEmpty(inputObj.name))
+            {
+                Console.WriteLine("Error: mssing data");
+                Console.WriteLine("Press any key to try again");
+                Console.ReadKey();
+                menu.MainMenu();
+            }
+            else if(inputObj.content.Contains(",") || inputObj.name.Contains(","))
+            {
+
+                
+                inputObj.content = inputObj.content.Replace(",", "");
+                inputObj.name = inputObj.name.Replace(",", "");
+                Console.WriteLine(inputObj.content);
+                goto done;
+            }
+            else {
+                goto done;
+            
+                 }
+            done:  string newString = $"{list}[{length}]{inputObj.name} - {inputObj.content},";
+            
+            File.WriteAllText(@"C:\Users\Måns\source\repos\moment3\guests.txt", newString);
+            Console.WriteLine("Quote added");
+        }
+
+
+
+     
+      
+            public static void Delete(string[] arr)
+        {
+
+            string newString = "";
+
+            Console.WriteLine("Choose index");
+            var input = Console.ReadLine();
+            int numIndex = int.Parse(input);
+           
+
+            arr = arr.Where((val, idx) => idx != numIndex).ToArray();
+            int j = 0;
+            foreach (string i in arr)
+            {
+
+                int startIndex = i.Length - 3;
+
+               string content = i.Substring(3, startIndex);
+
+                newString += "[" + j + "]" + content + ",";
+
+                j++;
+
+            }
+
+
+            
+            File.WriteAllText(@"C:\Users\Måns\source\repos\moment3\guests.txt", newString);
+
+           
+            Console.WriteLine("Quote deleted");
+
+        }
+    }
+}
---
+++ moment3/menu.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+
+namespace moment3
+{
+    class menu
+    {
+
+       public static void MainMenu()
+        {
+
+            //Clear the console and write out the options
+            Console.Clear();
+            Console.WriteLine("Choose");
+            Console.WriteLine("");
+            Console.WriteLine("Option 1. Write");
+            Console.WriteLine("Option 2. Delete");
+            Console.WriteLine("Option 3. Quit");
+            //Read the file
+            var a = File.ReadAllText(@"C:\Users\Måns\source\repos\moment3\guests.txt");
+
+            
+            //Put the file in an array and get its length
+            string[] fooArray = a.Split(',', StringSplitOptions.RemoveEmptyEntries);  // now you have an array of 3 strings
+
+            int length = fooArray.Length;
+
+      
+
+            //Write out the posts
+            foreach (string i in fooArray)
+            {
+                int j = 0;
+                Console.WriteLine(i);
+            }
+
+
+            String[] arr = new String[] { a };
+
+
+            //Let the user tpye in a number and start the correlating fucntion
+            string myoptions;
+            myoptions = Console.ReadLine();
+
+            switch (myoptions)
+            {
+                case "1":
+
+                    input.Write(a, length);
+                    MainMenu();
+
+                    break;
+
+                case "2":
+
+                    input.Delete(fooArray);
+                    MainMenu();
+
+                    break;
+
+                case "3":
+
+                    Exit();
+                    MainMenu();
+
+                    break;
+
+            }
+
+            
+            //Exit the app
+
+            static void Exit()
+            {
+                Environment.Exit(1);
+            }
+
+        }
+
+    }
+}
