a31dac8d8fab401b1db471f0e71f0cd41ec74651
Meuons <89832844+Meuons@users.noreply.github.com>
2021-11-12 13:56:45 +0100

    Update error.cs

--- moment3/error.cs
+++ moment3/error.cs
@@ -1,1 +1,19 @@
 
+using System;
+
+
+namespace moment3
+{
+    public class Error
+    {
+
+        public static void Empty(guest obj)
+        {
+            //Dispaly an error message and restart the write function
+            Console.WriteLine("Error: mssing data");
+            Console.WriteLine("Press any key to try again ");
+            Console.ReadKey();
+            guest.Write(); 
+        }
+    }
+}
